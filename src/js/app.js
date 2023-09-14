c14163a519f6ef1826a551edf6e3b0f966da46aa
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-09-14 00:27:23 +0000

    Fixed observations

--- src/js/app.js
+++ src/js/app.js
@@ -44,16 +44,16 @@ function render(variables = {}) {
           </h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
-              variables.twitter
+              variables.twitter == null ? "" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${
-              variables.github
+              variables.github == null ? "" : variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${
-              variables.linkedin
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin == null ? "" : variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
-              variables.instagram
+              variables.instagram == null ? "" : variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
